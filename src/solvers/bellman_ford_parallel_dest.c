7c8eca5ef33c6670efdd82233704df7007bec634
Mihail Petrov Stamenov - mihail.stamenov@studio.unibo.it <mihail.stamenov@cs.unibo.it>
2024-09-18 16:52:43 +0200

    start editing dest omp

--- src/solvers/bellman_ford_parallel_dest.c
+++ src/solvers/bellman_ford_parallel_dest.c
@@ -104,6 +104,43 @@ BFOutput *bellmanFord(int p, DestGraph *g, int startNode)
 
 int main()
 {
+    int numnodes, maxNumEdges;
+    int edgesCount[18];
+    for (int i = 0; i < 9; i++)
+    {
+        get_numbers(i, &numnodes, &maxNumEdges);
+        if (maxNumEdges == numnodes)
+        {
+            maxNumEdges = maxNumEdges - 1;
+        }
+        printf("For index = %d, numbers are %d, %d\n", i, numnodes, maxNumEdges);
+        DestGraph *destGNoCycle = createGraphNoNegativeCycle(numnodes, maxNumEdges);
+        char filenameNoCycle[50];
+        snprintf(filenameNoCycle, sizeof(filenameNoCycle), "../../data/graph_no_cycle_%d.edg_%d.txt", numnodes, destGNoCycle->numEdges);
+        writeGraphToFile(destGNoCycle, filenameNoCycle);
+        freeDestGraph(destGNoCycle);
+        edgesCount[2 * i] = destGNoCycle->numEdges;
+        DestGraph *destGCycle = createGraphWithNegativeCycle(numnodes, maxNumEdges);
+        char filenameCycle[50];
+        snprintf(filenameCycle, sizeof(filenameCycle), "../../data/graph_cycle_%d.edg_%d.txt", numnodes, destGCycle->numEdges);
+        writeGraphToFile(destGCycle, filenameCycle);
+        freeDestGraph(destGCycle);
+        edgesCount[2 * i + 1] = destGCycle->numEdges;
+    }
+    FILE *file = fopen("../../data/stats.txt", "w"); // Open file in write mode
+    if (file == NULL)
+    {
+        printf("Error opening file!\n");
+        return 0;
+    }
+
+    for (int i = 0; i < 18; i++)
+    {
+        fprintf(file, "%d\n", edgesCount[i]); // Write each integer to a new line
+    }
+
+    fclose(file);
+    
     DestGraph *readGraph = readDestGraphFromFile("../../data/graph_no_cycle_5.txt");
     BFOutput *result = bellmanFord(2, readGraph, 0);
     printf("---------------- %d\n", (*result).hasNegativeCycle);
